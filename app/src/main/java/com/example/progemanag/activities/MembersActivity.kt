fd3d90ba0eb88225181cb24a5ad7933ed56fad66
user <km_413@naver.com>
2021-06-05 17:31:21 +0900

    Adding New Member UI

--- app/src/main/java/com/example/progemanag/activities/MembersActivity.kt
+++ app/src/main/java/com/example/progemanag/activities/MembersActivity.kt
@@ -1,11 +1,16 @@
 package com.example.progemanag.activities
 
+import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.progemanag.R
 import com.example.progemanag.adapters.MemberListItemsAdapter
 import com.example.progemanag.databinding.ActivityMembersBinding
+import com.example.progemanag.databinding.DialogSearchMemberBinding
 import com.example.progemanag.firebase.FirestoreClass
 import com.example.progemanag.models.Board
 import com.example.progemanag.models.User
@@ -41,6 +46,44 @@ class MembersActivity : BaseActivity() {
             setHasFixedSize(true)
             adapter = MemberListItemsAdapter(this@MembersActivity, list)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add_member, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_add_member -> {
+                dialogSearchMember()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun dialogSearchMember() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_search_member)
+        val dialogBinding = DialogSearchMemberBinding.inflate(LayoutInflater.from(this))
+        dialogBinding.apply {
+            tvAdd.setOnClickListener {
+                val email = etEmailSearchMember.text.toString()
+                if(email.isNotEmpty()) {
+                    dialog.dismiss()
+                    // TODO implement adding memeber logic
+                } else {
+
+                }
+            }
+            tvCancel.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
+        dialog.show()
+
     }
 
     private fun setupActionbar() {
